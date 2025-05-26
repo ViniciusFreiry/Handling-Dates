0229f753e78a79f4a89839db1ad3e5dea205a025
ViniciusFreiry <viniciusfreiry@gmail.com>
2025-05-26 11:04:13 -0300

    Using LocalDate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        
+        LocalDate todayDate = LocalDate.now();
+        LocalDate firstInstallmentDate = LocalDate.of(2025, 8, 2);
+        LocalDate secondInstallmentDate = firstInstallmentDate.plusDays(30);
+
+        System.out.println("Today Date: " + todayDate);
+        System.out.println("First Installment Date: " + firstInstallmentDate);
+        System.out.println("Second Installment Date: " + secondInstallmentDate + "\n");
+
+        if (firstInstallmentDate.isEqual(todayDate)) {
+            System.out.println("Today is the due date!");
+        } else if (firstInstallmentDate.isBefore(todayDate)) {
+            System.out.println("Payment overdue.");
+        } else {
+            System.out.println("It's not the due date yet!");
+        }
     }
 }
